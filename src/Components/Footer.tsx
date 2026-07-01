f28f74ea4f113831f6a40b1995122c5856c7c69f
Joaquin Shugt <shugtfelipejoaquin@gmial.com>
2026-07-01 15:54:09 -0300

    e hola diganme si funciono, este es el footer prueba 1

---
+++ src/Components/Footer.tsx
@@ -0,0 +1,40 @@
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="footer__container">
+
+        <div className="footer__section">
+          <h3>AL palololo</h3>
+          <p>Una breve descripción del sitio web</p>
+        </div>
+
+        <div className="footer__section">
+          <h3>Enlaces</h3>
+          <ul>
+            <li><a href="/">Inicio</a></li>
+            <li><a href="/about">Nosotros</a></li>
+            <li><a href="/contact">Contacto</a></li>
+          </ul>
+        </div>
+
+        <div className="footer__section">
+          <h3>Redes Sociales</h3>
+          <ul>
+            <li><a href="#">Instagram</a></li>
+            <li><a href="#">Facebook</a></li>
+            <li><a href="#">X</a></li>
+          </ul>
+        </div>
+
+      </div>
+
+      <div className="footer__bottom">
+        <p>© 2026 Mi Sitio. Todos los derechos reservados.</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
