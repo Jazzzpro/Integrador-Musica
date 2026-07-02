ad2502327e6b9aeef6ec6808fd5a0751f029adb2
Jazzz <jazminrod724@gmail.com>
2026-07-02 15:10:36 -0300

    header by tizi

--- src/Components/Home.tsx
+++ src/Components/Home.tsx
@@ -4,12 +4,7 @@ import { Link } from "react-router-dom";
 const Home = () => {
   return (
     <div className="home-layout">
-      <header className="store-header text-center py-4 bg-light">
-        <h1 className="store-brand__name">Al palo</h1>
-      </header>
-
       <div className="home-content container py-4">
-        <p className="store-brand__tagline">En donde la música se escuche al palo</p>
         <main className="home-main">
           <ProductList limit={10} />
           <div className="text-center mt-4">
